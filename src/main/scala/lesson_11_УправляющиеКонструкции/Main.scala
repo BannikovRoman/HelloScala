ce82932a7a7d18554ee576019b28417d6e22f8d5
Роман <bannicovr64@gmail.com>
2019-07-14 22:19:05 +0400

    progress

--- src/main/scala/lesson_11_УправляющиеКонструкции/Main.scala
+++ src/main/scala/lesson_11_УправляющиеКонструкции/Main.scala
@@ -18,6 +18,12 @@ object Main {
   /* Считайте с клавиатуры число n и выведите на экран
   все упорядоченные пары взаимно простых чисел от 1 до n, не включая n, ﻿в лексикографическом порядке. */
   def main(args: Array[String]) {
-
+    val num = readInt()
+    for (i <- 1 until num) {
+      for (j <- 1 until num) {
+        if (BigInt(i).gcd(BigInt(j)).equals(1))
+          println(s"$i $j")
+      }
+    }
   }
 }
